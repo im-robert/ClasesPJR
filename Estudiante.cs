03ff4d552f082392b6abc2866e513a34bd20296d
Roberto Capellán <capellan1603@gmail.com>
2023-09-13 12:32:26 -0400

    practica terminada

--- Estudiante.cs
+++ Estudiante.cs
@@ -2,10 +2,16 @@
 {
     public class Estudiante : MiembroDeLaComunidad
     {
-        public string Matricula {  get; set; }
+        public string Matricula { get; set; }
 
-        public double calificaciones { get; set; }
+        public double Calificaciones { get; set; }
 
         public string Curso { get; set; }
+
+        
+    public string presentacion()
+        {
+            return $"Hola!, estudio {Curso}, mi matricula es {Matricula} y tuve una calificacion de {Calificaciones}";
+        } 
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ClasesPJR 
+{
+   public class program
+    {
+        static void Main(string[] args)
+        {
+            var estudiante = new Estudiante();
+            {
+                estudiante.Matricula = "20220950";
+
+                estudiante.Calificaciones = 90.5;
+
+                estudiante.Curso = "Software2";
+            }
+
+
+            Console.WriteLine(estudiante.presentacion());
+        }
+
+       
+    }
+
+ }
